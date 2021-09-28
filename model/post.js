4941aefef52913f00ed6fde79c429fe7c495217d
Techso <stevenlarex@gmail.com>
2021-09-28 12:10:08 +0100

    Added properties of post to post model

--- model/post.js
+++ model/post.js
@@ -8,6 +8,7 @@ const PostSchema = new mongoose.Schema({
 	author: { type: String, default: null, required: true },
 	postHasMedia: { type: Boolean, default: false },
 	postMedia: { type: Array, default: null },
+	noOfLikes: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model("post", PostSchema);
